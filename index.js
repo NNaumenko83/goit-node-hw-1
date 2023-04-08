ba2461449475888184e34d12282d08da19e87308
Nikolay Naumenko <nnv2006@ukr.net>
2023-04-08 10:23:55 +0300

    added functions to work with JSON

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-console.log("Hello");
-console.log("Hello again");
+const { Command } = require("commander");
+
+const program = new Command();
+const contacts = require("./contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const argv = program.opts();
+console.log("argv:", argv);
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const contactById = await contacts.getContactById(id);
+      return console.log("contactById:", contactById);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
